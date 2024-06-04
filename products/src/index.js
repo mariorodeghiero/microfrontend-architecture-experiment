1a08f556bd99a825e04856054e2f0cd6557dd967
Mario Rodeghiero <mario.rodeghiero@gmail.com>
2024-06-04 22:14:44 +0200

    [producs][s]: render fake products in the screen

--- products/src/index.js
+++ products/src/index.js
@@ -6,4 +6,4 @@ for (let i = 0; i < 5; i++) {
   products += `<div>${faker.commerce.productName()} - $${faker.commerce.price()}</div>`;
 }
 
-console.log(products);
+document.querySelector("#dev-products").innerHTML = products;
