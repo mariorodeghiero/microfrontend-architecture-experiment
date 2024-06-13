e488399becb3e9b91e6fcdbcb0aa7bfb19fa0909
Mario Rodeghiero <mario.rodeghiero@gmail.com>
2024-06-13 21:55:38 +0200

    [microFE][s]: refactor to run each microFE in isolation

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,4 +1,7 @@
-import "products/ProductsIndex";
-import "cart/CartShow";
+import { mount as productsMount } from "products/ProductsIndex";
+import { mount as cartMount } from "cart/CartShow";
 
 console.log("Hello from container");
+
+productsMount(document.querySelector("#microFE-products"));
+cartMount(document.querySelector("#microFE-cart"));
